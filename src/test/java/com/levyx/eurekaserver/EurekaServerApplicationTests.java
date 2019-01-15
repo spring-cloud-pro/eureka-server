c3ba93be241143b03080d363e50bd4ad80863f99
xingliwei <362493656@qq.com>
2019-01-15 15:18:58 +0800

    first commit

---
+++ src/test/java/com/levyx/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levyx.eurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class EurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
